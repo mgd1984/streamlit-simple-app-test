1e1733a303f57eb473896dac97d3217074f95e00
Alec M <alecjohnmiller@gmail.com>
2024-05-10 23:19:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ import pandas as pd
 
 import plotly.express as px
 
-# st.set_page_config(
-#     page_title="Ex-stream-ly Cool App",
-#     page_icon="🧊",
-#     layout="centered",
-#     initial_sidebar_state="collapsed",
-#     menu_items={
-#         'Get Help': 'https://www.extremelycoolapp.com/help',
-#         'Report a bug': "https://www.extremelycoolapp.com/bug",
-#         'About': "# This is a header. This is an *extremely* cool app!"
-#     }
-# )
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+)
 
 def main():
     data = {
